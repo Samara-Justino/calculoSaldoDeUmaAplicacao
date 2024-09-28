39653491405a858cd5bb1edfed905e956e88005d
Samara Justino <samarasouzajustino@gmail.com>
2024-09-28 17:57:06 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Qual o saldo da aplicação? ");
+double saldo = double.Parse(Console.ReadLine());
+saldo = saldo + (saldo * (15.0 / 100.0));
+Console.WriteLine(("O novo saldo é R$ ") + saldo.ToString("F2"));
